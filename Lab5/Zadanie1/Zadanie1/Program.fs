ad5888f691b3c4917b7d19d1fabdb813deaea7e4
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:20:26 +0100

    Lab5

---
+++ Lab5/Zadanie1/Zadanie1/Program.fs
@@ -0,0 +1,14 @@
+﻿let rec fibonacci n =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | _ -> fibonacci (n - 1) + fibonacci (n - 2)
+
+
+let fibonacciTailRecursive n =
+    let rec aux a b n =
+        match n with
+        | 0 -> a
+        | 1 -> b
+        | _ -> aux b (a + b) (n - 1)
+    aux 0 1 n
---
+++ Lab5/Zadanie2/Zadanie2/Program.fs
@@ -0,0 +1,33 @@
+﻿type 'a BinaryTree =
+    | Empty
+    | Node of 'a * 'a BinaryTree * 'a BinaryTree
+
+let rec searchRecursive tree value =
+    match tree with
+    | Empty -> false
+    | Node (v, left, right) ->
+        if v = value then true
+        else
+            searchRecursive left value || searchRecursive right value
+
+let searchIterative tree value =
+    let rec loop stack =
+        match stack with
+        | [] -> false
+        | Empty :: rest -> loop rest
+        | Node (v, left, right) :: rest ->
+            if v = value then true
+            else loop (left :: right :: rest)
+    loop [tree]
+
+
+let tree =
+    Node (10,
+        Node (5, Empty, Empty),
+        Node (15, Node (12, Empty, Empty), Node (20, Empty, Empty)))
+
+let resultRecursive = searchRecursive tree 12
+let resultIterative = searchIterative tree 12
+
+printfn "Wynik rekurencyjny: %b" resultRecursive
+printfn "Wynik iteracyjny: %b" resultIterative

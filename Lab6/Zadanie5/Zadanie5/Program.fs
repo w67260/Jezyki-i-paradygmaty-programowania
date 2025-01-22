5b8475b2b47b5a141529d10fc86865419ffcf91e
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:27:57 +0100

    Lab6

---
+++ Lab6/Zadanie5/Zadanie5/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let findLongestWord (text: string) =
+    let words = text.Split([| ' '; '\t'; '\n'; '\r' |], StringSplitOptions.RemoveEmptyEntries)
+    
+    let longestWord = 
+        words |> Array.maxBy (fun word -> word.Length)
+    
+    longestWord
+
+let processText () =
+    printfn "Podaj tekst:"
+    let inputText = Console.ReadLine()
+
+    let longestWord = findLongestWord inputText
+    
+    printfn "Najdłuższe słowo: %s" longestWord
+    printfn "Długość najdłuższego słowa: %d" longestWord.Length
+
+processText ()
---
+++ Lab6/Zadanie6/Zadanie6/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let replaceWord (inputText: string) (searchWord: string) (replaceWord: string) =
+    inputText.Replace(searchWord, replaceWord)
+
+let processText () =
+    printfn "Podaj tekst:"
+    let inputText = Console.ReadLine()
+
+    printfn "Podaj słowo, które chcesz wyszukać:"
+    let searchWord = Console.ReadLine()
+
+    printfn "Podaj słowo, na które chcesz zamienić:"
+    let newWord = Console.ReadLine()
+
+    let modifiedText = replaceWord inputText searchWord newWord
+
+    printfn "Zmodyfikowany tekst:"
+    printfn "%s" modifiedText
+
+processText ()

8f98027738ddf1014f319c45d449937cffa3886d
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:26:39 +0100

    Lab6

---
+++ Lab6/Zadanie3/Zadanie3/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let removeDuplicates (words: string list) =
+    words |> List.distinct
+
+printfn "Podaj słowa oddzielone spacjami:"
+let inputText = Console.ReadLine()
+
+let wordsList = inputText.Split([| ' ' |], System.StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+
+let uniqueWords = removeDuplicates wordsList
+
+printfn "Unikalne słowa:"
+uniqueWords |> List.iter (printfn "%s")
---
+++ Lab6/Zadanie4/Zadanie4/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let convertFormat (input: string) =
+    let parts = input.Split([| ';' |], System.StringSplitOptions.RemoveEmptyEntries)
+    
+    if parts.Length = 3 then
+        let firstName = parts.[0].Trim()
+        let lastName = parts.[1].Trim()
+        let age = parts.[2].Trim()
+        
+        sprintf "%s, %s (%s lat)" lastName firstName age
+    else
+        "Błędny format danych"
+
+let rec processEntries () =
+    printfn "Podaj dane w formacie 'imię; nazwisko; wiek' (lub wpisz 'koniec' aby zakończyć):"
+    let input = Console.ReadLine()
+    
+    if input.ToLower() = "koniec" then
+        printfn "Zakończono wprowadzanie danych."
+    else
+        let formattedText = convertFormat input
+        printfn "Przekształcony format: %s" formattedText
+        processEntries ()
+
+processEntries ()

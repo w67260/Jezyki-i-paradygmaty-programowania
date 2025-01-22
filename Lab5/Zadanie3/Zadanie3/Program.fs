a9d59ef2e503ed07ce909ef444d7a7ec86a7a5b1
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:21:41 +0100

    Lab5

---
+++ Lab5/Zadanie3/Zadanie3/Program.fs
@@ -0,0 +1,15 @@
+﻿let rec permutations list =
+    match list with
+    | [] -> [[]]
+    | head :: tail ->
+        let restPermutations = permutations tail
+        [ for perm in restPermutations do
+            for i in 0 .. List.length perm do
+                yield List.insertAt i head perm ]
+
+
+let numbers = [1; 2; 3]
+let perms = permutations numbers
+
+printfn "Permutacje listy:"
+perms |> List.iter (fun perm -> printfn "%A" perm)
---
+++ Lab5/Zadanie4/Zadanie4/Program.fs
@@ -0,0 +1,35 @@
+﻿let rec hanoi n source target auxiliary =
+    if n > 0 then
+        hanoi (n - 1) source auxiliary target
+        printfn "Przenieś dysk %d z %s na %s" n source target
+        hanoi (n - 1) auxiliary target source
+
+
+
+let hanoiIterative n source target auxiliary =
+    let totalMoves = pown 2 n - 1
+    let mutable moves = 0
+
+    let mutable source = source
+    let mutable target = target
+    let mutable auxiliary = auxiliary
+
+    if n % 2 = 0 then
+        let temp = target
+        target <- auxiliary
+        auxiliary <- temp
+
+    while moves < totalMoves do
+        if moves % 3 = 0 then
+            printfn "Ruch %d: Przenieś dysk z %s na %s" (moves + 1) source target
+        elif moves % 3 = 1 then
+            printfn "Ruch %d: Przenieś dysk z %s na %s" (moves + 1) source auxiliary
+        else
+            printfn "Ruch %d: Przenieś dysk z %s na %s" (moves + 1) auxiliary target
+
+        moves <- moves + 1
+
+
+
+hanoi 3 "A" "C" "B"
+hanoiIterative 3 "A" "C" "B"

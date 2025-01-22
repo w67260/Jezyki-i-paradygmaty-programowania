78411df2c93608cdd88dec0bd9351ee99a9f9606
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:24:53 +0100

    Lab6

---
+++ Lab6/Zadanie1/Zadanie1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let countWordsAndChars (text: string) =
+    let words = text.Split([| ' '; '\t'; '\n'; '\r' |], System.StringSplitOptions.RemoveEmptyEntries)
+    let wordCount = words.Length
+    
+    let charCount = text.Replace(" ", "").Length
+    
+    printfn "Liczba słów: %d" wordCount
+    printfn "Liczba znaków (bez spacji): %d" charCount
+
+printfn "Podaj tekst:"
+let inputText = Console.ReadLine()
+
+countWordsAndChars inputText
---
+++ Lab6/Zadanie2/Zadanie2/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let isPalindrome (text: string) =
+    let cleanText = text.Replace(" ", "").ToLower()
+    cleanText = String(Array.rev (cleanText.ToCharArray()))
+
+printfn "Podaj tekst:"
+let inputText = Console.ReadLine()
+
+if isPalindrome inputText then
+    printfn "Tekst jest palindromem."
+else
+    printfn "Tekst nie jest palindromem."

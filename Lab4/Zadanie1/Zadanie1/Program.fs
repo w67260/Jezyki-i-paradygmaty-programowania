abf830e622474c8c06aacf588c8b2520f513489c
w67260 <116088861+w67260@users.noreply.github.com>
2025-01-22 11:17:11 +0100

    Lab4

---
+++ Lab4/Zadanie1/Zadanie1/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+
+type UserData = { Weight: float; Height: float }
+
+let calculateBMI weight height = weight / (height / 100.0) ** 2.0
+
+let getBMICategory bmi =
+    match bmi with
+    | _ when bmi < 18.5 -> "Niedowaga"
+    | _ when bmi >= 18.5 && bmi < 24.9 -> "Waga prawidłowa"
+    | _ when bmi >= 25.0 && bmi < 29.9 -> "Nadwaga"
+    | _ -> "Otyłość"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Witaj! Ten program oblicza Twoje BMI."
+    printf "Podaj swoją wagę w kilogramach: "
+    let weightInput = Console.ReadLine()
+    printf "Podaj swój wzrost w centymetrach: "
+    let heightInput = Console.ReadLine()
+
+    try
+        let weight = float weightInput
+        let height = float heightInput
+
+        let userData = { Weight = weight; Height = height }
+
+        let bmi = calculateBMI userData.Weight userData.Height
+
+        let category = getBMICategory bmi
+
+        printfn "Twoje BMI wynosi: %.2f" bmi
+        printfn "Kategoria BMI: %s" category
+    with
+    | :? FormatException ->
+        printfn "Błąd: Wprowadzone dane muszą być liczbami."
+    | ex ->
+        printfn "Nieoczekiwany błąd: %s" ex.Message
+
+    0
